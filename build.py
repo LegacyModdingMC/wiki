3742ed78d872410decf18615124fb784b3292ba8
makamys <makamys@outlook.com>
2023-04-12 17:33:40 +0200

    Move templates out of docs dir
    This way they're excluded from the output

--- build.py
+++ build.py
@@ -7,7 +7,9 @@ docsDir = Path("docs")
 if not docsDir.is_dir():
     sys.exit("Couldn't find docs directory. Make sure you are running this script from the root directory of the repo.")
 
-for template in glob.glob("docs/*.template.md"):
+for template in glob.glob("docs-template/*.template.md"):
     print("Converting", template)
-    scripts.expand_favicon_links.convert(template, template.replace(".template", ""))
+    templatePath = Path(template)
+    outPath = templatePath.parent.parent / templatePath.parent.name.split("-")[0] / templatePath.name.replace(".template", "")
+    scripts.expand_favicon_links.convert(template, outPath)
 
